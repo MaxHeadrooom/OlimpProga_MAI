3d99151929a1b1f41d6dbcc05c07c34ab7461266
MaxHeadrooom <89976445+MaxHeadrooom@users.noreply.github.com>
2023-04-28 15:34:19 +0300

    Create B.cpp

---
+++ 1/B.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double a, b, c, temp;
+    cin >> a >> b >> c;
+    cout.precision(6);
+    cout <<fixed << (a + b)*c << endl;
+    return 0;
+}
